a0c4a41d235c1ff0a1728b6cb587a4d081c8d9f7
DanilWeda <danil.grigorets@gmail.com>
2023-10-26 21:39:00 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'public/bundle.[contenthash].js',
+		assetModuleFilename: 'images/[name].[hash][ext]',
+		clean: true,
+	},
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpack({
+			filename: 'index.html',
+			template: './src/index.html',
+		}),
+		new ESLintPlugin({
+			extensions: ['.js'],
+			quiet: true,
+		}),
+	],
+};
