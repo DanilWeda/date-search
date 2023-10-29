66469b907c327d15ba45fdf7b6661b49238a297b
DanilWeda <danil.grigorets@gmail.com>
2023-10-29 20:50:03 +0200

    Fixed some code style issues

---
+++ src/js/helpers/formatDate.js
@@ -0,0 +1,35 @@
+export const generateDaysClass = (propDay, currentValue, date) => {
+	const isActiveDay = currentValue === changeDateSeparator(date, true);
+	switch (true) {
+		case !propDay:
+			return 'off-day';
+		case isActiveDay:
+			return 'active';
+		default:
+			return '';
+	}
+};
+
+export const changeDateSeparator = (date, reverse = false, separator = '.') => {
+	if (!date) return;
+	const oppositeSeparator = separator === '-' ? '.' : '-';
+	const splitDate = reverse ? date.split(oppositeSeparator).reverse() : date.split(oppositeSeparator);
+	return splitDate.join(separator);
+};
+
+export const formatDate = (day, month, year, separator = '.') => {
+	return `${day}${separator}${month}${separator}${year}`;
+};
+
+export const getCorrectDayOrMonth = (dayOrMonth) => {
+	if (dayOrMonth < 10) return `0${dayOrMonth}`;
+	return dayOrMonth;
+};
+
+export const concatYearAndMonth = (year, month) => {
+	return `${year}-${month}`;
+};
+
+export const getNumFromDate = (date) => {
+	return date?.split('.').join('').split('_').join('');
+};
---
+++ src/js/user.js
@@ -0,0 +1,44 @@
+export class User {
+	constructor(data) {
+		this.user = data;
+		this.container = document.getElementById('card-section');
+		this.renderUser();
+	}
+
+	async renderUser() {
+		const { id, firstname = '-', lastname = '-', email = '-', phone = '-', birthday = '-', gender = '-', website = '-' } = this.user;
+		const renderBirthday = birthday.split().reverse().join().replace(/-/g, '.');
+		const userMarkup = `
+			<div id="user-${id}" class="card-container__card card">
+				<h2 class="card__name">${firstname} ${lastname}</h2>
+				<div class="card__inner">
+					<ul class="card__list">
+						<li class="card__item card__item--mail">
+							<span class="card__item-text">
+								<a href="mailto:${email}" class="card__item-link">${email}</a>
+							</span>
+						</li>
+						<li class="card__item card__item--phone">
+							<span class="card__item-text">
+								<a href="tel:${phone}" class="card__item-link">${phone}</a>
+							</span>
+						</li>
+						<li class="card__item card__item--birth">
+							<p class="card__item-text">${renderBirthday}</p>
+						</li>
+						<li class="card__item card__item--gender">
+							<p class="card__item-text">${gender}</p>
+						</li>
+						<li class="card__item card__item--site">
+							<p class="card__item-text">
+								<a href="${website}" target="_blank" class="card__item-link">${website}</a>
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		`;
+
+		this.container.insertAdjacentHTML('beforeend', userMarkup);
+	}
+}
